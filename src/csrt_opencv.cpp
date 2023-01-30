78e849d5a6d0413467854a27c934625eaa6793b7
egor <Egor Kiselev>
2023-01-30 18:57:32 +0300

    Initial commit

---
+++ src/csrt_opencv.cpp
@@ -0,0 +1,52 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        std::cout << "Usage: ./csrt <input_video_file> <output_video_file>" << std::endl;
+        return 1;
+    }
+
+    cv::VideoCapture video(argv[1]);
+
+    if(!video.isOpened()) {
+        std::cout << "Could not open video file" << std::endl; 
+        return 1;
+    } 
+
+    cv::Mat frame;
+
+    if (!video.read(frame)) {
+        std::cout << "Could not read video file" << std::endl; 
+        return 1;
+    }
+ 
+    cv::Rect_<int> rect = cv::selectROI(frame, false); 
+    cv::rectangle(frame, rect, cv::Scalar(255, 0, 0), 2);
+ 
+    cv::imshow("Tracker", frame);
+    
+    cv::Ptr<cv::TrackerCSRT> tracker = cv::TrackerCSRT::create();
+    tracker->init(frame, rect);
+
+    cv::VideoWriter video_writer(argv[2], cv::VideoWriter::fourcc('M','J','P','G'), 24, frame.size());
+     
+    while(video.read(frame)) {
+        if (tracker->update(frame, rect)) {
+            cv::rectangle(frame, rect, cv::Scalar(255, 0, 0), 2);
+        }
+
+        cv::imshow("Tracker", frame);
+
+        video_writer.write(frame);
+
+        if (cv::waitKey(1) == 'q') {
+            break;
+        }
+    }
+
+    video_writer.release();
+
+    return 0;
+}
