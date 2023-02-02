9c319537702fab5c8ce1c2648a95c2433bcb2c01
Egor Kiselev <kiselev.el@phystech.edu>
2023-02-02 22:50:09 +0300

    Channel convolution calculation separated

--- src/tracker_csrt.cpp
+++ src/tracker_csrt.cpp
@@ -2,23 +2,41 @@
 
 
 TrackerCSRT::TrackerCSRT(const cv::Mat& frame, const cv::Rect& bbox) :
-    filter(frame, bbox)
+    filter(frame, bbox),
+    channels(1),
+    channel_weights(1)
 {}
 
 
+void TrackerCSRT::getChannelFilter(const int channel_id, cv::Mat1f& filter) {
+    // Calculate the closed-form solution for channel filter using formula (4)
+}
+
+
+void TrackerCSRT::convolution(const cv::Mat1f& src, const cv::Mat1f& filter, cv::Mat1f& dst) {
+    cv::filter2D(src, dst, -1, filter, cv::Point(-1, -1), 0, cv::BORDER_WRAP);
+}
+
+
 bool TrackerCSRT::update(const cv::Mat& frame, cv::Rect& bbox) {
-    cv::Mat1f object;
-    cv::Mat1f convolution;
+    // Temporary use just grayscale channel with 1.0 weight
+    cv::Mat1f(frame, bbox).convertTo(channels[0], CV_32F);
+    channel_weights[0] = 1.0f;
 
-    cv::Mat1f(frame, bbox).convertTo(object, CV_32F);
+    cv::Mat1f resulting_convolution = cv::Mat::zeros(bbox.height, bbox.width, CV_32F);
+    cv::Mat1f channel_convolution;
 
-    cv::filter2D(object, convolution, -1, filter, cv::Point(-1, -1), 0, cv::BORDER_WRAP);
+    for (int channel_id = 0; channel_id < channels.size(); ++channel_id) {
+        getChannelFilter(channel_id, filter);
+        convolution(channels[channel_id], filter, channel_convolution);
+        resulting_convolution = resulting_convolution + channel_convolution * channel_weights[channel_id];
+    }
 
     double minVal;
     double maxVal;
     cv::Point minLoc;
     cv::Point maxLoc;
-    cv::minMaxLoc(convolution, &minVal, &maxVal, &minLoc, &maxLoc);
+    cv::minMaxLoc(resulting_convolution, &minVal, &maxVal, &minLoc, &maxLoc);
 
     bbox += (maxLoc - cv::Point(bbox.width / 2, bbox.height / 2));
 
--- src/tracker_csrt.hpp
+++ src/tracker_csrt.hpp
@@ -2,16 +2,56 @@
 #define TRACKER_CSRT
 
 
+#include <vector>
 #include <opencv2/opencv.hpp>
 
 
+/**
+ * \brief Class for CSRT tracking algorithm
+ */
 class TrackerCSRT {
 public:
+
+    /**
+     * \brief Constructs and initializes tracker
+     * 
+     * \param[in] frame First frame of the video
+     * \param[in] bbox Initial bounding box of the tracked object
+     */
     TrackerCSRT(const cv::Mat& frame, const cv::Rect& bbox);
+
+    /**
+     * \brief Makes one step of algorithm
+     * Updates current bounding box according to the transformation of the object in the next frame
+     * 
+     * \param[in] frame Next frame of the video
+     * \param[in] bbox Bounding box of the tracked object in the previous frame
+     * \return true - updated successfully, false - tracking failed
+     */
     bool update(const cv::Mat& frame, cv::Rect& bbox);
 
 private:
+    
+    /**
+     * \brief Calculates convolution filter for channel
+     * 
+     * \param[in] channel_id Channel index
+     * \param[in] filter Result destination
+     */
+    void getChannelFilter(const int channel_id, cv::Mat1f& filter);
+
+    /**
+     * \brief Convolves an image with the filter
+     * 
+     * \param[in] src Input image
+     * \param[in] filter Convolution kernel
+     * \param[in] dst Output image of the same size as src
+     */
+    void convolution(const cv::Mat1f& src, const cv::Mat1f& filter, cv::Mat1f& dst);
+
     cv::Mat1f filter;
+    std::vector<cv::Mat1f> channels;
+    std::vector<float> channel_weights;
 };
 
 
